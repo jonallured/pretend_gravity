ec90d0994f2a0a2b254f97b0d7e43dda5da2f87b
Jon Allured <jon@jonallured.com>
2024-03-05 10:46:55 -0600

    Add grape endpoint to create an artwork

--- app/api/using_grape/artworks_endpoint.rb
+++ app/api/using_grape/artworks_endpoint.rb
@@ -10,6 +10,16 @@ module UsingGrape
       get ":id" do
         Artwork.find(params[:id])
       end
+
+      post do
+        artwork = Artwork.new(params)
+        if artwork.save
+          artwork
+        else
+          errors = {errors: artwork.errors.full_messages.to_sentence}
+          error! errors, 400
+        end
+      end
     end
   end
 end
---
+++ spec/requests/using_grape/create_artwork_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "POST /api/using_grape/artworks" do
+  context "without required params" do
+    it "returns a 400 and the errors" do
+      params = {}
+      post "/api/using_grape/artworks", params: params
+      expect(response.status).to eq 400
+      expect(response.parsed_body.key?("errors")).to eq true
+    end
+  end
+
+  context "with required params" do
+    it "returns a 201 and the new artwork" do
+      params = {
+        amount_cents: 100_000,
+        artist_name: "Sally Sculptor",
+        medium: "Clay",
+        title: "Fancy cup"
+      }
+      post "/api/using_grape/artworks", params: params
+      expect(response.status).to eq 201
+      artwork = Artwork.last
+      expect(response.parsed_body).to eq artwork.as_json
+    end
+  end
+end
